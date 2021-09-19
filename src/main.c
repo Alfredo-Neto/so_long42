13df345a61c801d3870281e06bd95e7e76db5f55
Alfredo Braule <alfredoneto934@gmail.com>
2021-09-19 14:21:46 -0300

    new examples

--- src/main.c
+++ src/main.c
@@ -1,34 +1,23 @@
 #include "so_long.h"
 
-//Since key_press() can recieve only one argument, all the argument shold be gathered in one structure
-//x,y and str are meaningless variables.
-typedef struct s_param{
-	int		x;
-	int		y;
-	char	str[3];
-}				t_param;
-
-//Only param->x will be used. 
-void			param_init(t_param *param)
+int				key_press(int keycode, void *param)
 {
-	param->x = 3;
-	param->y = 4;
-	param->str[0] = 'a';
-	param->str[1] = 'b';
-	param->str[2] = '\0';
-}
+	// static int a = 0;
 
-int				key_press(int keycode, t_param *param)
-{
-	static int a = 0;
+	// if (keycode == 'w')//Action when W key pressed
+	// 	param->x++;
+	// else if (keycode == 's') //Action when S key pressed
+	// 	param->x--;
+	// else if (keycode == XK_Escape) //Quit the program when ESC key pressed
+	// 	exit(0);
 
-	if (keycode == 'w')//Action when W key pressed
-		param->x++;
-	else if (keycode == 's') //Action when S key pressed
-		param->x--;
-	else if (keycode == XK_Escape) //Quit the program when ESC key pressed
+	if (keycode == XK_Escape)
+	{
+		printf("tecla: %d\n", keycode);
 		exit(0);
-	printf("x: %d\n", param->x);
+	}
+		
+	printf("tecla: %d\n", keycode);
 	return (0);
 }
 
@@ -36,18 +25,19 @@ int			main(void)
 {
 	void		*mlx;
 	void		*win;
-	t_param		param;
+	void		*img;
+	int		img_width;
+	int		img_height;
 
-	param_init(&param);
+
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 500, 500, "mlx_project");
-	printf("-------------------------------\n");
-	printf("'W key': Add 1 to x.\n");
-	printf("'S key': Subtract 1 from x\n");
-	printf("'ESC key': Exit this program\n");
-	printf("'Other keys': print current x \n");
-	printf("-------------------------------\n");
-	printf("Current x = 3\n");
-	mlx_key_hook(win, &key_press, &param);
+	img = mlx_xpm_file_to_image(mlx, "./textures/wall_n.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(mlx, win, img, 50, 50);
+	printf("-----------------------------------------------------------------------\n");
+	printf("Pressione qualquer tecla, e seu número correspondente será mostrado\n");
+	printf("'Tecla ESC': Sair do programa\n");
+	printf("-----------------------------------------------------------------------\n");
+	mlx_hook(win, 2, 1L<<0, &key_press, (void *)0);
 	mlx_loop(mlx);
 }
