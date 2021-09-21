967ab8457676da00da5b0c9a8c7e05d1cb2d9f3f
Alfredo Braule <alfredoneto934@gmail.com>
2021-09-21 02:23:00 -0300

    printing character by character of the map

--- src/so_long.c
+++ src/so_long.c
@@ -4,16 +4,23 @@ int main (int argc, char **argv)
 { 
     char **map;
     int i;
+    int j;
 
     map = NULL;
 
-    i = 0;
     if (argc == 2)
     {
         map = read_map(argv[1]);
+        i = 0;
         while(map[i])
         {
-            printf("%s\n", map[i]);
+            j = 0;
+            while(map[i][j])
+            {   
+                printf("%c", map[i][j]);
+                j++;
+            }
+            printf("\n");
             i++;
         }
     }
