6dcc37ad333554e7bd3734ec2e710bd780716c31
Alfredo Braule <alfredoneto934@gmail.com>
2021-10-05 17:55:24 -0300

    moved comments out of main

--- src/so_long.c
+++ src/so_long.c
@@ -1,18 +1,17 @@
 #include "so_long.h"
+// todo:
+    // destruir(dar free) no mapa, e tudo o mais, para nao continuar o jogo depois da saida
+    // verificar se o mapa é válido
+    // abstrair funcoes da main para outras(arquitetura)
+    // fechar janela ao clicar no x
+    // opcional: mostrar stats do jogo na propria janela, ao invés de apenas no shell
+
+    // bonus: criar tropas inimigas
+    //        animar colecionaveis
 
 int main (int argc, char **argv)
 { 
     t_game   game;
-
-    // todo:
-        // destruir(dar free) no mapa, e tudo o mais, para nao continuar o jogo depois da saida
-        // verificar se o mapa é válido
-        // abstrair funcoes da main para outras(arquitetura)
-        // fechar janela ao clicar no x
-        // opcional: mostrar stats do jogo na propria janela, ao invés de apenas no shell
-
-        // bonus: criar tropas inimigas
-        //        animar colecionaveis
 
     if (argc == 2)
     {
