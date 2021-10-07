55dffc5201a584eeb8fe66b54390607a5476a7fc
Alfredo Braule <alfredoneto934@gmail.com>
2021-10-06 21:01:06 -0300

    created and tested has_valid_chars

--- src/map_check.c
+++ src/map_check.c
@@ -26,6 +26,28 @@ static int has_valid_walls(char **map, t_map *m)
     return (1);
 }
 
+static int has_valid_chars(char **map)
+{
+    int  i;
+    int  j;
+
+    i = 0;
+    while(map[i])
+    {
+        j = 0;
+        while(map[i][j])
+        {   
+           if (map[i][j] != '1' && map[i][j] != '0' 
+            && map[i][j] != 'P' && map[i][j] != 'C' 
+            && map[i][j] != 'E')
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
+
 int is_valid_map(char **map)
 {
     t_map m;
@@ -33,7 +55,7 @@ int is_valid_map(char **map)
     //     && has_minimum_chars() && is_rectangular() && has_valid_extension())
     //     return (1);
 
-    if (has_valid_walls(map, &m))
+    if (has_valid_walls(map, &m) && has_valid_chars(map))
         return (1);
     return (0);
 }
