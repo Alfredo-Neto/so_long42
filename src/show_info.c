7dd49061a32cac16fe24a8f7a69d8253fab69dc2
Alfredo Braule <alfredoneto934@gmail.com>
2021-10-15 02:00:51 -0300

    displaying only moves in mandatory part

--- src/show_info.c
+++ src/show_info.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 14:44:39 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/10/13 17:03:53 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/10/15 02:00:03 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,4 @@
 void	show_info(t_game *game)
 {
 	printf("Moves: %d\n", game->moves);
-	printf("Collectibles: %d, Collected: %d",
-		game->collectibles, game->collected);
-	print_map(game->map);
 }
