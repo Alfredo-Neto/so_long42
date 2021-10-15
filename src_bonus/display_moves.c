721e295f5c9052bbedff19a21784c90683a7c39d
Alfredo Braule <alfredoneto934@gmail.com>
2021-10-15 01:42:35 -0300

    changed display move color to red

--- src_bonus/display_moves.c
+++ src_bonus/display_moves.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 00:41:20 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/10/15 01:36:35 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/10/15 01:40:52 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	display_moves(t_game *game)
 	num = ft_itoa(game->moves);
 	moves = ft_strjoin("MOVES: ", num);
 	mlx_string_put(game->mlx, game->win, 1 * SPRITE_SIZE, 1.5 * SPRITE_SIZE,
-		0x000000, moves);
+		0xFF0000, moves);
 	ft_super_free((void *)&moves);
 	ft_super_free((void *)&num);
 	return (0);
